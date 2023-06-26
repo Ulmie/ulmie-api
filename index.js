34e4ce80a94b34ebe543a990de0308439a18a2d8
Ulmie <ulmie1257@gmail.com>
2023-06-27 02:52:22 +0300

    first commit9

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import cookieParser from "cookie-parser";
 
 //middlewares
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", true);
   next();
 });
 app.use(express.urlencoded({ extended: false }));
