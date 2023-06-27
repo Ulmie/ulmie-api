6e777bd4c9d176968d6db91225ee16f51dd09d67
Ulmie <ulmie1257@gmail.com>
2023-06-27 04:32:47 +0300

    first commit12347

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ import multer from "multer";
 import cookieParser from "cookie-parser";
 
 //middlewares
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-//     next();
-// });
-// app.use(express.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+});
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(
   cors()
